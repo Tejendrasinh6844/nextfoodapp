bbddfa3228fa15772310cdea5397b75b7a3a5e68
Tejendrasinh <tejendrasinh6844@gmail.com>
2024-02-08 08:49:55 +0530

    tbv

--- src/components/Featured.tsx
+++ src/components/Featured.tsx
@@ -1,11 +1,29 @@
+import Image from 'next/image'
 import React from 'react'
 
 const Featured = () => {
   return (
-    <div>
-      Featured
-Featured
-    </div>
+    <div className=" w-screen overflow-x-scroll text-red-500">
+    {/* Wrapper */}
+      <div className="w-max flex">
+{/* single item */}
+<div className="w-screen h-[60vh] flex flex-col items-center justify-around p-4">
+  {/* image container */}
+  <div className="relative flex-1 w-full ">
+    <Image src="/temporary/p1.png" alt='' fill  className='object-contain'/>
+  </div>
+  {/* text container */}
+  <div className=" flex-1 flex flex-col gap-4 ">
+    <h1 className="text-xl font-bold uppercase ">text</h1>
+    <p>Desc</p>
+    <span>123</span>
+    <button className='bg-red-500 text-white p-2 rounded-md'>Add to Cart</button>
+  </div>
+</div>
+
+    
+      </div>
+      </div>
   )
 }
 
