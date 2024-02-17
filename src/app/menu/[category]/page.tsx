9d225692c3c5c6c67a48cd8d5732d74997879e76
Tejendrasinh <tejendrasinh6844@gmail.com>
2024-02-17 06:47:53 +0530

    17FEB update2024

--- src/app/menu/[category]/page.tsx
+++ src/app/menu/[category]/page.tsx
@@ -1,11 +1,28 @@
+import { pizzas } from '@/data';
+import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react'
 
-const page = () => {
+const CategoryPage = () => {
   return (
-    <div>
-      categorypage
+    <div className='flex flex-wrap text-red-500'>
+      {pizzas.map(item=>(
+        <Link href={`/product/${item.id}`} key={item.id} className='w-full md:w-1/2 h-[60vh] border-r-2 border-b-2 border-red-500 '>
+{/* image container */}
+{item.img && 
+<div className=' relative'>
+  <Image src={item.img} alt='' fill />
+  </div>}
+
+{/* text  container */}
+
+
+
+
+        </Link>
+      ))}
     </div>
   )
 }
 
-export default page
+export default CategoryPage;
