dfc0a94d5049fd834f2f9fc36d0f5ab96723b1e6
Tejendrasinh <tejendrasinh6844@gmail.com>
2024-02-24 07:28:13 +0530

    ttt

--- src/app/menu/[category]/page.tsx
+++ src/app/menu/[category]/page.tsx
@@ -7,16 +7,25 @@ const CategoryPage = () => {
   return (
     <div className='flex flex-wrap text-red-500'>
       {pizzas.map(item=>(
-        <Link href={`/product/${item.id}`} key={item.id} className='w-full sm:w-1/2 lg:w-1/3  h-[60vh] border-r-2 border-b-2 border-red-500 '>
+        <Link href={`/product/${item.id}`} key={item.id} className='w-full sm:w-1/2 lg:w-1/3  h-[60vh] border-r-2 border-b-2 border-red-500 p-4  flex flex-col justify-between group even:bg-fuchsia-100'>
 {/* image container */}
 {item.img && 
 <div className=' relative h-[80%]'>
-  <Image src={item.img} alt='' fill />
+  <Image src={item.img} alt='' fill  className=' object-contain'/>
   </div>}
 
 {/* text  container */}
 
+<div className="flex items-center justify-between font-bold ">
 
+<h1 className='text-2xl uppercase p-2'>{item.title}</h1>
+<h2 className='group-hover:hidden text-xl'>${item.price}</h2>
+<button className=' group-hover:block hidden uppercase bg-red-500 text-white p-2 rounded-md'>Add to Cart</button>
+
+
+
+
+</div>
 
 
         </Link>
