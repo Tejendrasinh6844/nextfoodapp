74f0cda496a544cbbf99dd609e539f5452689fd9
Tejendrasinh <tejendrasinh6844@gmail.com>
2024-02-28 22:58:08 +0530

    fontend done

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -5,7 +5,7 @@ const CartPage = () => {
   return (
     <div className='h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] flex flex-col text-red-500 lg:flex-row'>
    {/* Product container  */}
-<div className="h-1/2 p-4 flex flex-col justify-center overflow-scroll lg:h-full lg:w-2/3 2xl:w-1/2">
+<div className="h-1/2 p-4 flex flex-col justify-center overflow-scroll lg:h-full lg:w-2/3 2xl:w-1/2 lg:px-20 xl:px-40 ">
   {/* single item container  */}
   <div className=" flex items-center justify-between mb-4 ">
     <Image src="/temporary/p1.png" alt='' height={100} width={100} />
@@ -19,7 +19,7 @@ const CartPage = () => {
 </div>
 
    {/* Payment container  */}
-   <div className="h-1/2 p-4 bg-fuchsia-50 flex flex-col gap-4 justify-center lg:h-full lg:w-1/3 2xl:w1/2">
+   <div className="h-1/2 p-4 bg-fuchsia-50 flex flex-col gap-4 justify-center lg:h-full lg:w-1/3 2xl:w1/2 lg:px-20 xl:px-40 2xl:text-xl 2xl:gap-6">
     <div className="flex justify-between">
       <span>Subtotal (3 items)</span>
       <span>$81.70</span>
--- src/app/orders/page.tsx
+++ src/app/orders/page.tsx
@@ -1,11 +1,41 @@
 import React from 'react'
 
-const page = () => {
+const OrderPage = () => {
   return (
-    <div>
-      ordersPage
+    <div className='p-4 lg:px-20 xl:px-40'>
+     <table className=' w-full border-separate border-spacing-3'>
+      <thead className=' text-left'>
+        <tr>
+<th className=' hidden md:block'>Order Id</th>
+<th>Date</th>
+<th>Price</th>
+<th className=' hidden md:block'>Products</th>
+<th>Status</th>
+        </tr>
+      </thead>
+      
+      <tbody>
+<tr className=' text-sm md:text-base bg-red-50'>
+  <td className=' hidden md:block py-6 px-1'>7226866844</td>
+  <td className='py-6 px-1'>29-2-2024</td>
+  <td className='py-6 px-1'>89.90</td>
+  <td className=' hidden md:block py-6 px-1'>Big Burger, Veggie Pizza</td>
+  <td className='py-6 px-1'>Status</td>
+</tr>
+<tr className=' text-sm md:text-base odd:bg-gray-100'>
+  <td className=' hidden md:block py-6 px-1'>7226866844</td>
+  <td className='py-6 px-1'>29-2-2024</td>
+  <td className='py-6 px-1'>89.90</td>
+  <td className=' hidden md:block py-6 px-1'>Big Burger, Veggie Pizza</td>
+  <td className='py-6 px-1'>Status</td>
+</tr>
+      </tbody>
+     </table>
+
+
+
     </div>
   )
 }
 
-export default page
+export default OrderPage
